9c3635254264583856dd348bc29959f9709bcb69
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-21 00:00:04 -0400

    Finished get_drinks route

--- backend/src/api.py
+++ backend/src/api.py
@@ -20,13 +20,25 @@ CORS(app)
 
 ## ROUTES
 '''
-@TODO implement endpoint
+@ implement endpoint
     GET /drinks
         it should be a public endpoint
         it should contain only the drink.short() data representation
     returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
         or appropriate status code indicating reason for failure
 '''
+
+@app.route('/drinks')
+def get_drinks():
+    drinks = Drink.query.all()
+
+    try:
+        return jsonify({
+            "success": True,
+            "drinks": [drink.short() for drink in drinks]
+        })
+    except:
+        abort(404)
 
 
 '''

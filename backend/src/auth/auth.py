3b40e3c74e98aa4d74a70688e8d389227f3c8ee1
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-20 23:47:48 -0400

    Added auth0 domain and api audience

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -5,9 +5,11 @@ from jose import jwt
 from urllib.request import urlopen
 
 
-AUTH0_DOMAIN = 'udacity-fsnd.auth0.com'
+AUTH0_DOMAIN = 'fsndauthorization.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'dev'
+API_AUDIENCE = 'drinks'
+
+
 
 ## AuthError Exception
 '''
@@ -18,6 +20,7 @@ class AuthError(Exception):
     def __init__(self, error, status_code):
         self.error = error
         self.status_code = status_code
+
 
 
 ## Auth Header

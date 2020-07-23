d0730c7db1b94290e8fd128c80ceb9082b4e189c
Armanya Dalmia <armanya.dalmia@gmail.com>
2020-07-23 02:08:24 -0400

    Updated variables for Ionic frontend

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,4 @@
-/* @TODO replace with your variables
+/* replace with your variables
  * ensure all variables on this page match your project
  */
 
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'fsndauthorization.us', // the auth0 domain prefix
+    audience: 'drinks', // the audience set for the auth0 app
+    clientId: '0aewYiXO6PL2U4QTq7iLRBMp86avkF6V', // the client id generated for the auth0 app
+    callbackURL: 'http://127.0.0.1:8100', // the base url of the running ionic application. 
   }
 };
